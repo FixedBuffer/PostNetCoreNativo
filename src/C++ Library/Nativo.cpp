aae8c3bb1b4927a84d36df334d2c0f98f40287ce
JorTurFer <jorge_turrado@hotmail.es>
2018-12-10 19:57:26 +0100

    Añado el codigo y los cmakes

---
+++ src/C++ Library/Nativo.cpp
@@ -0,0 +1,6 @@
+#include "Nativo.h"
+#include <iostream>
+
+void MetodoNativo() {
+    std::cout << "Escribo esto desde C++" << std::endl;
+}
---
+++ src/PostNetCoreNativo/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace PostNetCoreNativo
+{
+    class Program
+    {
+        [DllImport(@"EjemploNativo.dll",EntryPoint = "MetodoNativo")]
+        public static extern void Windows_MetodoNativo();
+        [DllImport(@"EjemploNativo.so", EntryPoint = "MetodoNativo")]
+        public static extern void NoWindows_MetodoNativo();
+
+
+        static void MetodoNativo()
+        {
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+                Windows_MetodoNativo();
+            else
+                NoWindows_MetodoNativo();
+        }
+
+        static void Main(string[] args)
+        {
+            string OS = RuntimeInformation.OSDescription;
+            Console.WriteLine($"Escribo esto desde C# sobre {OS}");
+            MetodoNativo();
+            Console.Read();
+        }
+    }
+}
