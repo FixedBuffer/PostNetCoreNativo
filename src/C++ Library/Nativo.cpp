6e7746e978c8385152c5513bb32306a77d260dc0
JorTurFer <jorge_turrado@hotmail.es>
2018-12-23 17:50:28 +0100

    Documento el codigo C++

--- src/C++ Library/Nativo.cpp
+++ src/C++ Library/Nativo.cpp
@@ -4,12 +4,14 @@
 
 
 void GetStringMessage(char* str, int strlen) {
+	//Definimos el mensaje
 	std::string result = "Mensaje generado desde C++";
-
+	//Cortamos lo que sobre si el buffer que nos pasan es pequeño
 	result = result.substr(0, strlen);
-
+	//Copiamos el mensaje en el buffer de salida
 	std::copy(result.begin(), result.end(), str);
-	str[std::min(strlen - 1, (int)result.size())] = 0;
+	//Colocamos un '\0' en la ultima posición, ya que es el delimitador de strings nativas
+	str[std::min(strlen - 1, (int)result.size())] = '\0';
 }
 
 int Suma(int A, int B) {
--- src/C++ Library/Nativo.h
+++ src/C++ Library/Nativo.h
@@ -2,9 +2,12 @@
 extern "C" {
 #endif
 
+//Utilizamos directivas de preprocesado para definir la macro de la API
+//Esto hay que hacerlo porque en Windows y en NoWindows se declaran diferente
+
 #ifdef _WIN32
 #  ifdef MODULE_API_EXPORTS
-#    define MODULE_API __declspec(dllexport)
+#    define MODULE_API __declspec(dllexport) 
 #  else
 #    define MODULE_API __declspec(dllimport)
 #  endif
@@ -12,6 +15,7 @@ extern "C" {
 #  define MODULE_API
 #endif
 
+//Declaracion de los métodos nativos
 MODULE_API void GetStringMessage(char* str, int strlen);
 
 MODULE_API int Suma(int A, int B);
